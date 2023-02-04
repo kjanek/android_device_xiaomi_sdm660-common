900c798fb5dbb177d51eb5caf700020fdd60ea15
menorziin <williamsilvapppl@gmail.com>
2023-02-04 17:44:05 +0100

    sdm660-common: build android.frameworks.sensorservice@1.0 HAL

--- sdm660.mk
+++ sdm660.mk
@@ -365,7 +365,9 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor
 
 # Tethering
 PRODUCT_PACKAGES += \
